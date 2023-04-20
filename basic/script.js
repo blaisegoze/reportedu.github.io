6bc7814de199469a1f4888456b70342b618e8b9f
Nathanael McClure <97613508+McGibborim@users.noreply.github.com>
2023-04-20 11:22:48 -0500

    Delete basic directory

--- basic/script.js
+++
@@ -1,6 +0,0 @@
-const modeToggleBtn = document.getElementById('mode-toggle-btn');
-const body = document.body;
-
-modeToggleBtn.addEventListener('click', () => {
-  body.classList.toggle('dark-mode');
-});
