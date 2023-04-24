4d8b1d156e73c8b57c09113fe0285d5c28dc8dfa
DeepFriedChumChum <XxTheDoctor1102xX@gmail.com>
2023-04-24 15:47:05 -0500

    Actually big push

--- index.php
+++ index.php
@@ -440,26 +440,28 @@ $to_date = $_POST['to_date'];
 $to_time = $_POST['to_time']; 
 
 // process the person data
-$full_name = $_POST['last_name' . ' ' . 'first_name' . ' ' . 'first_name'];
+$first_name = $_POST['first_name'];
+$middle_name = $_POST['middle_name'];
+$last_name = $_POST['last_name'];
 $ssn = $_POST['ssn'];
 $di_travel = $_POST['di_travel'];
 $street_address = $_POST['street_address'];
-$phone = $_POST['phonenum'];
-$city = $_POST['pers_city'];
-$state = $_POST['pers_state'];
-$zip_code = $_POST['pers_zip_code'];
+$phonenum = $_POST['phonenum'];
+$pers_city = $_POST['pers_city'];
+$pers_state = $_POST['pers_state'];
+$pers_zip_code = $_POST['pers_zip_code'];
 $date_of_birth = $_POST['date_of_birth'];
 $race = $_POST['race'];
 $sex = $_POST['sex'];
 
 // process the vehicle data
 $involvement = $_POST['involvement'];
-$driver_lic_no = $_POST['driver'];
-$lic_year = $_POST['year'];
-$state_vehicle = $_POST['veh_state'];
-$vehicle_year = $_POST['veh_year'];
+$driver = $_POST['driver'];
+$year = $_POST['year'];
+$veh_state = $_POST['veh_state'];
+$veh_year = $_POST['veh_year'];
 $make = $_POST['make'];
-$model = $_POST['model_veh'];
+$model_veh = $_POST['model_veh'];
 $style = $_POST['style'];
 $color = $_POST['color'];
 $vin = $_POST['vin'];
@@ -475,7 +477,7 @@ $value = $_POST['value'];
 $description = $_POST['description'];
 
 // process the modus operandi data
-$gang_act = isset($_POST['vehicle3']) ? 'Yes' : 'No';
+$gang_act = isset($_POST['gang_act']) ? 'Yes' : 'No';
 $means_of_attack = $_POST['attack'];
 $method_of_entry = $_POST['entry'];
 $weapon_used = $_POST['weapon'];
@@ -487,11 +489,10 @@ $victims_age = $_POST['victims_age'];
 
 // process the narrative data
 $summary = $_POST['summary'];
-$narrative = $_POST['n_narrative'];
+$n_narrative = $_POST['n_narrative'];
 
 // Insert the values into the database
-$query = "INSERT INTO ticket_info VALUES('$full_name', '$ssn', '$di_travel', '$street_address', '$phone', '$city', '$state', '$zip_code', '$date_of_birth')";
-mysqli_query($mysqli, $query); 
+$query = "INSERT INTO ticket_info VALUES('$report_num', '$author', '$report_date', '$report_time', '$nat_call', '$location', '$inc_city', '$inc_state', '$inc_zip', '$from_date', '$from_time', '$to_date', '$to_time', '$first_name', '$middle_name', '$last_name', '$ssn', '$di_travel', '$street_address', '$phonenum', '$pers_city', '$pers_state', '$pers_zip_code', '$date_of_birth', '$race', '$sex', '$involvement', '$driver', '$year', '$veh_state', '$veh_year', '$make', '$model_veh', '$style', '$color', '$vin', '$involvement_prop', '$article', '$brand', '$model_prop', '$serial_num', '$owner', '$value', '$description', '$gang_act', '$means_of_attack', '$method_of_entry', '$weapon_used', '$premis_type', '$num_victims', '$victims_race', '$victims_sex', '$victims_age', '$summary', '$n_narrative')";
 }
 
 
